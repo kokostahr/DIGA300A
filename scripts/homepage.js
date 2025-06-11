89c6116ff887a57c55938be8ef0a04153b481e2e
kokostahr <starking.nn@gmail.com>
2025-06-11 19:10:48 +0200

    why r u broken? Y ARE U BROKEN??? Caro

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -6,13 +6,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("nextBtn");
     //starting at 0 for the items in the caro
     let currentIndex = 0;
-    //variable and function that will make the caro move 
+    //findind the width of each caro item 
+    let slideWidth;
+
+    //ensuring that width is calculated after all the caro items are loaded in
+    window.addEventListener("load", () => {
+        slideWidth = items[0].getBoundingClientRect().width;
+        //position each item horizontally
+        items.forEach((item, index) => {
+            item.style.left = `${slideWidth * index}px`;
+        })
+    })
     
+    //variable and function that will make the caro move 
     function updateCarousel() {
-        const width = track.getBoundingClientRect().width;
-        track.style.transform = `translateX(-${width * currentIndex}px)`;
+        //const width = track.getBoundingClientRect().width;
+        /**had an issue where the items were stacking in one carousel slide, making a change to this code to 
+         * ensure that the js gets the APPROPRIATE width of each item! ples work
+         */
+        track.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
     }
-    updateCarousel();
     console.log('prev', prevButton);
     console.log('next', nextButton);
 
@@ -20,17 +33,33 @@ document.addEventListener("DOMContentLoaded", function () {
     nextButton.addEventListener("click", () => {
         currentIndex = (currentIndex + 1) % items.length;
         updateCarousel();
+        resetAutoplay();
     });
 
     prevButton.addEventListener("click", () => {
         currentIndex = (currentIndex - 1 + items.length) % items.length;
         updateCarousel();
+        resetAutoplay();
     })
 
-    //AN OPTIONAL JUICY THING TO MAKE IT SLIDE EVERY 3 SECONDS. Need to later add something that ensures it also considers the user's clicks then starts counting
-    setInterval(() => {
+    //AN OPTIONAL JUICY THING TO MAKE IT SLIDE EVERY 3 SECONDS. 
+       /* setInterval(() => {
         currentIndex = (currentIndex + 1) % items.length;
         updateCarousel();
-    }, 3000);
+    }, 3000); adding the autoplay feature so that i can reset it when users
+    click the arrows*/
+    let autoPlay = setInterval(() => {
+        currentIndex = (currentIndex + 1) % items.length;
+        updateCarousel();
+    }, 4000);
+
+    //reset the autoplay timer everytime user clicks
+    function resetAutoplay() {
+        clearInterval(autoPlay);
+        autoPlay = setInterval(() => {
+            currentIndex = (currentIndex + 1) % items.length;
+            updateCarousel();
+        }, 4000);
+    }
 
 })
